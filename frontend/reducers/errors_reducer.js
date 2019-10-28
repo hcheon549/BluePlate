503c33653be626da15127f8bec02bdc12460710e
Eric Cheon <hcheon549@gmail.com>
2019-10-27 20:54:47 -0400

    Plans backend

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -3,12 +3,14 @@ import sessionErrorsReducer from './session_errors_reducer';
 import mealErrorsReducer from './meal_errors_reducer';
 import schoolErrorsReducer from './school_errors_reducer';
 import userErrorsReducer from './user_errors_reducer';
+import planErrorsReducer from './plan_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer,
   users: userErrorsReducer,
   meals: mealErrorsReducer,
-  schools: schoolErrorsReducer
+  schools: schoolErrorsReducer,
+  plans: planErrorsReducer
 });
 
 export default errorsReducer;
