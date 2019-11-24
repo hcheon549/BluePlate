d6a8df40b5b15b179d9e478b4063a9c45be32af6
Eric Cheon <hcheon549@gmail.com>
2019-11-23 23:23:03 -0500

    fixing

--- frontend/components/reservations/reservations.jsx
+++ frontend/components/reservations/reservations.jsx
@@ -16,7 +16,6 @@ const TIMEVALS = ['11:00','11:30','12:00','12:30',
 '13:00','13:30','14:00','14:30','15:00','15:30',
 '16:00','16:30'];
 
-
 class Reservations extends React.Component {
   constructor(props) {
     super(props);
