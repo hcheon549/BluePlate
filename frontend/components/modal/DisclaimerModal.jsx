c89b76b469d4fc11ef89e24f67bfa83d69b85b3d
Eric Cheon <hcheon549@gmail.com>
2019-12-08 18:28:43 -0500

    Updating disclaimer wording

--- frontend/components/modal/DisclaimerModal.jsx
+++ frontend/components/modal/DisclaimerModal.jsx
@@ -35,10 +35,10 @@ class DisclaimerModal extends React.Component {
         <div className="innerContent">
           <h4>Disclaimer</h4>
           <p><span className="miniText">
-            Any information and actions in this page including name and location restaurants , menu items and reservations are for demonstration purposes. BluePlattr or restaurants do not recognize reservations made in this page, and will not honor for any claim made in this page.
+            Any information and actions in this page including, but not limited to, name and location of restaurants, menu items and reservations are for demonstration purposes. BluePlattr or restaurants will not recognize or honor reservations or any claims made in this page.
           </span></p>
           <input type="checkbox"  name="signed" checked={this.state.signed} value="" onChange={this.toggleAgreeButton} />
-            <label value="agreed">I have read and agree to the disclaimer.</label>
+          <label>I have read and agree to the disclaimer.</label>
           <button className={'orangeSecondary' + (this.state.signed ? '' : ' disabled')} type="submit" onClick={this.proceed}>Check it out!</button>
         </div>
       </div>
