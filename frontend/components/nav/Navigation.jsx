a9bdb55825a1119795dd864f536562dbf39a4414
Eric Cheon <hcheon549@gmail.com>
2019-12-10 14:26:58 -0500

    Navigation wip

--- frontend/components/nav/Navigation.jsx
+++ frontend/components/nav/Navigation.jsx
@@ -6,7 +6,7 @@ import NavLogin from "./NavLogin";
 import MenuItem from "./MenuItem";
 import StepIndicator from './StepIndicator'
 
-const Navigation = (props) => {
+const Nav = (props) => {
   let navSelect = props.location.pathname === "/users/signup" ? (
     <StepIndicator activeStep={props.stepJoin}/>
   ) : (props.loggedIn && props.location.pathname !== '/demo') ? (
@@ -33,6 +33,50 @@ const Navigation = (props) => {
     </div>
     )
 };
+
+const Burger = (props) => {
+  return (
+    <div className="navLogo">
+      <Link to="/">
+        <img src="https://blueplate-development.s3.amazonaws.com/logo.png" alt="logo" />
+        <span className="theLogo">BluePlattr</span>
+      </Link>
+    </div>
+  )
+}
+
+class Navigation extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      isMobile: window.innerWidth <= 800
+    }
+
+    this.handleResize = this.handleResize.bind(this);
+  }
+
+  componentDidMount() {
+		this.handleResize();
+		window.addEventListener('resize', this.handleResize);
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleResize);
+	}
+
+	handleResize() {
+		if (window.innerWidth <= 800 && this.state.isMobile === false) {
+			this.setState({ isMobile: true });
+		} else if (window.innerWidth > 800 && this.state.isMobile === true) {
+			this.setState({ isMobile: false });
+		}
+	}
+
+  render(){
+    let navigation = this.state.isMobile ? <Burger {...this.props} /> : <Nav {...this.props} />
+    return navigation
+  }
+}
 
 const mapStateToProps = state => {
   let { currentUser } = state.entities
