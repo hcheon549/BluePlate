3b41e3242edab54f194e1ed01861485ee4225a04
Eric Cheon <hcheon549@gmail.com>
2019-12-15 23:01:37 -0500

    Adding development config for Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,18 +65,19 @@ Rails.application.configure do
   # Automatically inject JavaScript needed for LiveReload
   # config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
 
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.default charset: "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "blueplattr.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "support@blueplattr.com",
-    password: "Wjsguswo1122!!"
-  }
-
+    domain: 'smtp.gmail.com',
+    user_name: ENV["BLUEPLATTR_MAIL_USERNAME"],
+    password: ENV["BLUEPLATTR_MAIL_PASSWORD"],
+    authentication: :login,
+    enable_starttls_auto: true
+  } 
+  
   config.action_mailer.default_url_options = { host: "localhost:3000" }
 end
