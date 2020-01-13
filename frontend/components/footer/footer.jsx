6457c1014d8234567b44b15d72f0d8f815e8a77c
Eric Cheon <hcheon549@gmail.com>
2020-01-13 00:13:04 -0500

    updating email wip

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -11,7 +11,7 @@ const Footer = (props) => (
       <span />
       <li className="miniText" onClick={() => props.history.push('/privacy-policy')}>Privacy Policy</li>
       <span />
-      <li className="miniText"><a href="mailto:partner@blueplattr.com">Become a vendor</a></li>
+      <li className="miniText"><a href="mailto:hello@blueplattr.com">Become a vendor</a></li>
     </ul>
     <nav>
       <div style={{textAlign: "center"}}>All rights reserved<br/>&copy; BluePlans Inc.</div>
