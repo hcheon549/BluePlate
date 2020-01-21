f15b83c72f9d48030f54d6866a2a6721f08f7e29
Eric Cheon <hcheon549@gmail.com>
2020-01-20 19:46:49 -0500

    fixing facebook link

--- frontend/components/footer/SocialMedia.jsx
+++ frontend/components/footer/SocialMedia.jsx
@@ -6,7 +6,7 @@ const SocialMedia = () => {
     <div class="socialMedia">
       <ul class="icons">
         <a href="https://www.instagram.com/blue_plattr/" target="_blank"><i class="fab fa-instagram"></i></a>
-        <a href="www.facebook.com/BluePlattr" target="_blank"><i class="fab fa-facebook-square"></i></a>
+        <a href="https://www.facebook.com/BluePlattr" target="_blank"><i class="fab fa-facebook-square"></i></a>
         <a href="https://twitter.com/blueplattr" target="_blank"><i class="fab fa-twitter-square"></i></a>
       </ul>
     </div>
