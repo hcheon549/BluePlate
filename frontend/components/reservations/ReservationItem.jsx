0e48a2c1c22196e01344f9ccbc59dd59a80df9f7
Eric Cheon <hcheon549@gmail.com>
2020-01-26 00:38:27 -0500

    Remove reservation update button after kitchen is closed

--- frontend/components/reservations/ReservationItem.jsx
+++ frontend/components/reservations/ReservationItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import moment from 'moment';
 
 const ReservationItem = (props) => {
 
   let { type, hasMeal, reservation, pickupTime, openReserveModal } = props;
   let imageUrl, content;
+  let currentHour = moment().hour();
+  console.log(currentHour)
+  let canUpdate = type == "lunch" ? (currentHour < 10) : (currentHour < 16)
 
   let data = {
     menu: {...reservation.menu, meal: {...reservation.meal}},
@@ -12,7 +16,7 @@ const ReservationItem = (props) => {
     pickupTimeId: reservation.pickupTimeId,
     currentReservation: reservation
   }
-  
+
   if (hasMeal){
     imageUrl = reservation.meal.imageUrl;
     content = (
@@ -22,8 +26,8 @@ const ReservationItem = (props) => {
         <li className="shop-address">{reservation.shop.address}</li>
         <li className="pickup-time">Pick up between {reservation.pickupTime.start} and {reservation.pickupTime.end}</li>
         <li style={{display: 'flex', justifyContent: 'left'}}>
-          <span onClick={() => openReserveModal({ action: 'update', ...data})}>Update</span>
-          <span style={{marginLeft: '20px'}} onClick={() => openReserveModal({action: 'cancel', ...data})}>Cancel</span>
+          {canUpdate && <span style={{marginRight: '20px'}} onClick={() => openReserveModal({ action: 'update', ...data})}>Update</span>}
+          <span onClick={() => openReserveModal({action: 'cancel', ...data})}>Cancel</span>
         </li>
       </ul>
     )
