c8b52b9b7d18f52aabbbfd5680fb6019a9a8dce5
Eric Cheon <hcheon549@gmail.com>
2020-01-26 09:51:14 -0500

    wip

--- frontend/components/modal/ReservationModal.jsx
+++ frontend/components/modal/ReservationModal.jsx
@@ -88,13 +88,12 @@ class ReservationModal extends React.Component {
           action,
           code: reservationResult.reservation.pickupCode,
         })
-      } 
+      } else {
+        console.log(reservationResult)
+      }
       // else if (reservationResult.errors) {
       //   openConfirmModal({ action: 'create-error',})
       // } 
-      else{
-        console.log(reservationResult)
-      }
     }
     
     else if (action == 'update' && currentReservation) {
@@ -113,13 +112,13 @@ class ReservationModal extends React.Component {
         })
         // this.openSwal(action)
         // closeModal();
-      } 
+      } else {
+        console.log(updateResult)
+      }
       // else if (updateResult.errors) {
       //   openConfirmModal({ action: 'update-error',})
       // }
-      else {
-        console.log(updateResult)
-      }
+      
     }
     
     else if (action == 'cancel' && currentReservation){
